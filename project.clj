10d5972ac14768c5d2a280421101ded2faa28f30
Oliver Marshall <oliver-marshall@hotmail.com>
2017-11-12 19:40:17 +0000

    Mark project as version 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lan-van "0.0.0"
+(defproject lan-van "1.0.1"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
                  [expound "0.3.1"]
